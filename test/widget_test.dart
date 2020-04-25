9d382a9512d5a2e46845f0505d2b2ddf122a1c1d
ganievrenat <ganiev.renat@gmail.com>
2020-04-25 18:12:18 +0300

    new branch

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:FlutterGalleryApp/models/user.dart';
-import 'package:FlutterGalleryApp/user_holder.dart';
-import 'package:flutter/material.dart';
+import '../lib/models/user.dart';
+import '../lib/user_holder.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:FlutterGalleryApp/main.dart';
-import 'package:FlutterGalleryApp/app.dart';
 
 void main() {
   UserHolder holder;
